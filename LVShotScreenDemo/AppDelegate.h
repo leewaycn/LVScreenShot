fc61dcbc7ce087bb191d22fc6d242f1b4ad72134
Lewis.Li <lewis.li@esgo.cc>
2018-05-08 16:04:00 +0800

    Initial Commit

---
+++ LVShotScreenDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LVShotScreenDemo
+//
+//  Created by 孔友夫 on 2018/5/8.
+//  Copyright © 2018年 LV. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LVShotScreenDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LVShotScreenDemo
+//
+//  Created by 孔友夫 on 2018/5/8.
+//  Copyright © 2018年 LV. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
