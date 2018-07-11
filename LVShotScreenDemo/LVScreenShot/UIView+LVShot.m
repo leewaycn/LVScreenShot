f2059e31fc5ff1a09c984040bf1978e9d3de0838
Lewis.Li <lewis.li@esgo.cc>
2018-07-11 17:44:37 +0800

    add

--- LVShotScreenDemo/LVScreenShot/UIView+LVShot.m
+++ LVShotScreenDemo/LVScreenShot/UIView+LVShot.m
@@ -64,6 +64,50 @@ static const NSString * DDGViewScreenShotKey_IsShoting = @"DDGViewScreenShotKey_
     CGContextSaveGState(context);
     CGContextTranslateCTM(context, -self.frame.origin.x, -self.frame.origin.y);
 
+
+    if ([self isKindOfClass:[UIWindow class]]) {
+        CGSize imageSize = CGSizeZero;
+        UIInterfaceOrientation orientation = [UIApplication sharedApplication].statusBarOrientation;
+        if (UIInterfaceOrientationIsPortrait(orientation))
+            imageSize = [UIScreen mainScreen].bounds.size;
+        else
+            imageSize = CGSizeMake([UIScreen mainScreen].bounds.size.height, [UIScreen mainScreen].bounds.size.width);
+
+        UIGraphicsBeginImageContextWithOptions(imageSize, NO, 0);
+        CGContextRef context = UIGraphicsGetCurrentContext();
+        for (UIWindow *window in [[UIApplication sharedApplication] windows])
+        {
+            CGContextSaveGState(context);
+            CGContextTranslateCTM(context, window.center.x, window.center.y);
+            CGContextConcatCTM(context, window.transform);
+            CGContextTranslateCTM(context, -window.bounds.size.width * window.layer.anchorPoint.x, -window.bounds.size.height * window.layer.anchorPoint.y);
+            if (orientation == UIInterfaceOrientationLandscapeLeft)
+            {
+                CGContextRotateCTM(context, M_PI_2);
+                CGContextTranslateCTM(context, 0, -imageSize.width);
+            }
+            else if (orientation == UIInterfaceOrientationLandscapeRight)
+            {
+                CGContextRotateCTM(context, -M_PI_2);
+                CGContextTranslateCTM(context, -imageSize.height, 0);
+            } else if (orientation == UIInterfaceOrientationPortraitUpsideDown) {
+                CGContextRotateCTM(context, M_PI);
+                CGContextTranslateCTM(context, -imageSize.width, -imageSize.height);
+            }
+            if ([window respondsToSelector:@selector(drawViewHierarchyInRect:afterScreenUpdates:)])
+            {
+                [window drawViewHierarchyInRect:window.bounds afterScreenUpdates:YES];
+            }
+            else
+            {
+                [window.layer renderInContext:context];
+            }
+            CGContextRestoreGState(context);
+        }
+
+
+    }else
+
     if([ self DDGContainsWKWebView ]){
         [self drawViewHierarchyInRect:bounds afterScreenUpdates:YES];
     }else{
